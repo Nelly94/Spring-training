6dd02a4643c5113445b7dd2a2a69d06ae6950b73
nellyiriberri <nellyiriberri@gmail.com>
2020-06-08 11:48:31 +0200

    Replace bean declarations by component and autowired annotations

--- src/main/java/com/application/Guitarist.java
+++ src/main/java/com/application/Guitarist.java
@@ -1,12 +1,17 @@
 package com.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Guitarist implements IMusician{
 
+    @Autowired
     private IInstrument instrument;
 
-    public Guitarist(IInstrument instrument) {
+    /*public Guitarist(IInstrument instrument) {
         this.instrument = instrument;
-    }
+    }*/
 
     public IInstrument getInstrument() {
         return instrument;
--- src/main/java/com/application/IInstrument.java
+++ src/main/java/com/application/IInstrument.java
@@ -1,4 +1,7 @@
 package com.application;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface IInstrument {
 }
